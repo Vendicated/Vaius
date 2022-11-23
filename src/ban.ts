c4abeb3025e358392c92a19b7dc53c2955cdc559
Vendicated <vendicated@riseup.net>
2022-11-23 19:09:12 +0100

    Multi ban

--- src/ban.ts
+++ src/ban.ts
@@ -5,16 +5,33 @@ const idRe = /^(?:<@!?)?(\d{17,20})>?$/;
 
 export default defineCommand({
     name: "ban",
-    async execute(msg, user, reason) {
+    async execute(msg, ...args) {
         if (!msg.inCachedGuildChannel()) return;
 
         if (!msg.member.permissions.has("BAN_MEMBERS"))
             return reply(msg, { content: "nop" });
 
-        const id = idRe.exec(user ?? "")?.[1];
-        if (id)
-            return msg.guild.createBan(id, { reason: `${msg.author.tag}: ${reason || "Absolutely beaned"}` })
-                .then(() => reply(msg, { content: "Absolutely beaned" }))
-                .catch(err => reply(msg, { content: String(err) }));
+        const ids = [] as string[];
+        let reason = "Absolutely beaned";
+        for (let i = 0; i < args.length; i++) {
+            const id = args[i].match(idRe)?.[1];
+            if (id) {
+                ids.push(id);
+            } else {
+                reason = args.slice(i).join(" ");
+                break;
+            }
+        }
+        if (!ids.length) return reply(msg, { content: "Gimme some users silly" });
+
+        reason = `${msg.author.tag}: ${reason}`;
+
+        const results = [] as string[];
+        for (const id of ids) {
+            await msg.guild.createBan(id, { reason })
+                .catch(e => results.push(`Failed to ban ${id}: \`${String(e)}\``));
+        }
+
+        return reply(msg, { content: results.join("\n") || "Done!" });
     }
 });
