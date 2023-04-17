2b0ab0c02da8d2f577b811ea83209cb3fef29ef6
Vendicated <vendicated@riseup.net>
2023-04-17 22:58:47 +0200

    Add prune

---
+++ src/prune.ts
@@ -0,0 +1,40 @@
+import { Message } from "oceanic.js";
+
+import { defineCommand } from "./Command";
+import { reply } from "./util";
+
+export default defineCommand({
+    name: "prune",
+    async execute(msg, amount, modifier, extra) {
+        if (!msg.inCachedGuildChannel()) return;
+
+        if (!msg.member.permissions.has("MANAGE_MESSAGES"))
+            return reply(msg, { content: "nuh-uh!!" });
+
+        const limit = Number(amount);
+        if (!limit) return reply(msg, { content: "?" });
+
+        const filter: ((msg: Message) => boolean) | undefined = (() => {
+            switch (modifier) {
+                case "by":
+                case "from":
+                    if (extra === "bots") return m => m.author.bot;
+
+                    const id = extra.match(/\d+/)?.[0];
+                    return m => m.author.id === id;
+                case "embeds":
+                    return m => m.embeds.length > 0;
+                case "files":
+                case "attachments":
+                    return m => !m.attachments.empty;
+                case "invites":
+                    return m => m.content.includes("discord.gg/") || m.content.includes("discord.com/invite/");
+            }
+        })();
+
+        await msg.channel.purge({
+            limit,
+            filter
+        });
+    }
+});
